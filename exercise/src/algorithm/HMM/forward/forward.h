74ba1b83bcb3ed209728b15fc765691c5b4a99b9
wymhfut <weiyuanjiebing@sina.com>
2015-12-01 13:37:04 +0800

    [Refactor] 修改hmm的算法

--- exercise/src/algorithm/HMM/forward/forward.h
+++ exercise/src/algorithm/HMM/forward/forward.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "hmm.h"
+
 class CForward
 {
 public:
@@ -11,5 +13,40 @@ public:
 	{
 	}
 
+	void process(HMM* pHmm, int T,
+				 int* pObserve,
+				 float** ppAlpha,/*local prob, row: T, col: hidden state*/
+				 float* pProb)
+	{
+		int nHiddenStateNum = pHmm->getHiddenStateNum();
+		int nOutputStateNum = pHmm->getOutputStateNum();
+		
+		const float** ppOutputMat = pHmm->getOutputMat();
 
+		/*init alpha[0]*/
+		const float* pInitState = pHmm->getInitState();
+		int nCurOutState = pObserve[0];
+		for (int nHiddStIdx = 0; nHiddStIdx < nHiddenStateNum; nHiddStIdx++)
+		{
+			/*for cache*/
+			ppAlpha[0][nHiddStIdx] = pInitState[nHiddStIdx]*ppOutputMat[nCurOutState][nHiddStIdx];
+			pProb[0] += ppAlpha[0][nHiddStIdx];
+		}
+
+		/*induce*/
+		const float** ppTransMat = pHmm->getTransMat();
+		for(int nTIdx = 1; nTIdx < T; nTIdx++)
+		{
+			nCurOutState = pObserve[nTIdx];
+			for (int nHiddStIdx = 0; nHiddStIdx < nHiddenStateNum; nHiddStIdx++)
+			{
+				for(int nPreHiddStIdx = 0; nPreHiddStIdx < nHiddenStateNum; nPreHiddStIdx++)
+				{
+					ppAlpha[nTIdx][nHiddStIdx] += ppAlpha[nTIdx-1][nPreHiddStIdx]*ppTransMat[nPreHiddStIdx][nHiddStIdx];
+				}
+				ppAlpha[nTIdx][nHiddStIdx] *= ppOutputMat[nCurOutState][nHiddStIdx];
+				pProb[nTIdx] += ppAlpha[nTIdx][nHiddStIdx];
+			}
+		}
+	}
 };
--- exercise/src/algorithm/HMM/forward/hmm.h
+++ exercise/src/algorithm/HMM/forward/hmm.h
@@ -11,16 +11,52 @@ class HMM
 public:
 	HMM(const char* pHmmParaFile) : nHiddenStateNum_(0), nOutputStateNum_(0)
 	{
-
+		initPara(pHmmParaFile);
 	}
 
 	~HMM()
 	{
 	}
 
+	int getHiddenStateNum() const
+	{
+		return nHiddenStateNum_;
+	}
+
+	int getOutputStateNum() const
+	{
+		return nOutputStateNum_;
+	}
+
+	const float** getTransMat() const
+	{
+		return ppTransMat_;
+	}
+
+	const float** getOutputMat() const
+	{
+		return ppOutputMat_;
+	}
+
+	const float* getInitState() const
+	{
+		return pInitState_;
+	}
+
 private:
 	void initPara(const char* pHmmParaFile)
 	{
+		nHiddenStateNum_ = 3;
+		nOutputStateNum_ = 4;
+		/*ppTransMat_ = new float*[nHiddenStateNum_]();
+		for(int nHiddStIdx = 0; nHiddStIdx < nHiddenStateNum_; nHiddStIdx++)
+		{
+			for (int nNextHiddStIdx = 0; nNextHiddStIdx < nHiddenStateNum_; nNextHiddStIdx++)
+			{
+				ppTransMat_[nHiddStIdx][nNextHiddStIdx] = 
+			}
+		}*/
+		
 		ifstream inFile(pHmmParaFile);
 		string strCurLine;
 		while(getline(inFile, strCurLine))
